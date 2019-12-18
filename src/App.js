97442971f3bfed4d3c896463b44eea64e02b0ebd
Caspar Boetes <caspar@puukboek.local>
2019-12-18 17:35:35 +0100

    get geolocation from navigator

--- src/App.js
+++ src/App.js
@@ -1,8 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 import './App.css'
 
-function App() {
-  return <div className='App'></div>
-}
+export default () => {
+  const [position, setPosition] = useState()
+  const [latitude, setLatitude] = useState()
+  const [longitude, setLongitude] = useState()
+  const [error, setError] = useState()
 
-export default App
+  const onPositionRecieved = position => {
+    console.log(position)
+    setPosition(position.coords.latitude)
+    setLatitude(position.coords.latitude)
+    setLongitude(position.coords.longitude)
+  }
+
+  const onPositionNotRecieved = positionError => {
+    console.log(positionError.message)
+    setError(positionError.message)
+  }
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      onPositionRecieved,
+      onPositionNotRecieved,
+      {
+        timeout: 2,
+      }
+    )
+  } else {
+    console.log('Geolocation blocked')
+  }
+
+  return (
+    <div className='App'>
+      <code>
+        {error !== 'Timeout expired' ? (
+          <h4>{error}</h4>
+        ) : (
+          <h4>
+            {position}&nbsp;{latitude}&nbsp;{longitude}
+          </h4>
+        )}
+      </code>
+    </div>
+  )
+}
